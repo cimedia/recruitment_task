ab89321e0a73205589781404d3d2c2086440859f
Kamil Kierzkowski <kamil.kierzkowski@stxnext.pl>
2023-10-17 00:06:53 +0200

    - Additional method to remove HTML tags (without any libraries)

--- services/html_parser.py
+++ services/html_parser.py
@@ -5,3 +5,20 @@ class HTMLParser:
     @staticmethod
     def remove_html_tags(html: str):
         return re.sub("<.*?>", "", html)
+
+    @staticmethod
+    def remove_html_tags_v2(html: str):
+        tag = False
+        quote = False
+        output = []
+        for ch in html:
+            if ch == '<' and not quote:
+                tag = True
+            elif ch == '>' and not quote:
+                tag = False
+            elif (ch == '"' or ch == "'") and tag:
+                quote = not quote
+            elif not tag:
+                output.append(ch)
+
+        return "".join(output)
